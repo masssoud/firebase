1e959e0484dbc018bbbebb9e5f89d77b86407abb
m.najafi <massoudir.najafi@gmail.com>
2018-04-30 16:57:28 +0430

    init

---
+++ app/src/main/java/com/example/mnajafi/firebase/TextInputLayout.java
@@ -0,0 +1,14 @@
+package com.example.mnajafi.firebase;
+
+/**
+ * Created by m.najafi on 4/29/2018.
+ */
+//public class TextInputLayout {
+//    public void setErrorEnabled(boolean errorEnabled) {
+//        this.errorEnabled = errorEnabled;
+//    }
+//
+//    public void setError(String error) {
+//        this.error = error;
+//    }
+//}
